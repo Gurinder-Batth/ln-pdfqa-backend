e8c50016a28fc0f8ba91142d9d93bf8db3844c5c
Baraich <baraichaman@gmail.com>
2024-07-24 20:34:47 +0530

    ability to create new users

--- src/src/api.py
+++ src/src/api.py
@@ -1,5 +1,6 @@
-from .schemas import UserSchema
+from .schemas import UserSchema, UserCreationResponseSchema
 
+from django.contrib.auth.models import User
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.authentication import JWTAuth
 from ninja_jwt.controller import NinjaJWTDefaultController
@@ -11,3 +12,15 @@ api.register_controllers(NinjaJWTDefaultController)
 @api.get("/me", auth=JWTAuth(), response=UserSchema)
 def me(request):
     return request.user
+
+
+@api.post("/auth/create-user", response=UserCreationResponseSchema)
+def create_user(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+
+    user = User(username=username)
+    user.set_password(password)
+
+    user.save()
+    return dict(status="OK")
--- src/src/schemas.py
+++ src/src/schemas.py
@@ -4,3 +4,7 @@ from ninja import Schema
 class UserSchema(Schema):
     id: int
     email: str
+
+
+class UserCreationResponseSchema(Schema):
+    status: str
