55b846966a255cdb1ffc5d6909754149ad84b955
Baraich <baraichaman@gmail.com>
2024-07-24 21:21:27 +0530

    chat creation complete

--- src/pdfqa/api.py
+++ src/pdfqa/api.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from typing import List
 
 from ninja_extra import Router
 from ninja_jwt.authentication import JWTAuth
 
 from .models import Chat
-from .schemas import ChatSchemaList
+from .schemas import ChatSchemaList, ChatSchemaCreate
 
 router = Router()
 
@@ -12,3 +13,18 @@ router = Router()
 @router.get("", auth=JWTAuth(), response=List[ChatSchemaList])
 def get_chats(request):
     return Chat.objects.filter(user=request.user)
+
+
+@router.post("/create", auth=JWTAuth())
+def create_chat(request, data: ChatSchemaCreate):
+    _id = 1
+    last_chat = Chat.objects.last()
+
+    if last_chat is not None:
+        _id = last_chat.id + 1
+
+    chat = Chat(**data.dict(), id=_id, user=request.user, created_at=datetime.now().isoformat(),
+                updated_at=datetime.now().isoformat())
+    chat.save()
+
+    return {"id": _id}
--- src/pdfqa/schemas.py
+++ src/pdfqa/schemas.py
@@ -1,7 +1,18 @@
+from datetime import datetime
+
 from ninja import Schema
 
 
 class ChatSchemaList(Schema):
+    id: int
+    pdf_name: str
+    pdf_url: str
+    file_key: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class ChatSchemaCreate(Schema):
     pdf_name: str
     pdf_url: str
     file_key: str
